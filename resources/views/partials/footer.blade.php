09b87ac1fe0b90a29e91a403eff4825fc16e960c
Santos Ivan Alva Chavez <ivanals8998@gmail.com>
2021-01-07 18:14:22 +0100

    laravel - comics - backend

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+{{-- 
+    MAIN SITE HERE
+ --}}
+
+<footer class="main-footer">FOOTER HERE</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,46 @@
+{{-- 
+    MAIN SITE HERE
+ --}}
+
+<header class="main-header">
+    <div class="container">
+        <a class="navbar-brand" href="{{ route('home') }}">
+            <img src="{{ asset('images/logo.png') }}" alt="dc comics">
+        </a>
+
+        <nav class="navbar-nav">
+            <ul>
+                <li>
+                    <a href="#">Character</a>
+                </li>
+                <li>
+                    <a href="#">Comics</a>
+                </li>
+                <li>
+                    <a href="#">Movies</a>
+                </li>
+                <li>
+                    <a href="#">Tv</a>
+                </li>
+                <li>
+                    <a href="#">Games</a>
+                </li>
+                <li>
+                    <a href="#">Collectibles</a>
+                </li>
+                <li>
+                    <a href="#">Videos</a>
+                </li>
+                <li>
+                    <a href="#">Fans</a>
+                </li>
+                <li>
+                    <a href="#">News</a>
+                </li>
+                <li>
+                    <a href="#">Shop</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
